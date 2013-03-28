713be2b7024663e8d94229d1262ab6f0d3b3c9d1
Max <maxime.benoit27@gmail.com>
2013-03-28 17:03:53 +0100

    AjoutOperateur

--- src/java/client/AjoutOperateur.java
+++ src/java/client/AjoutOperateur.java
@@ -22,7 +22,9 @@ import metier.OperateurService;
  */
 @WebServlet(name = "AjoutOperateur", urlPatterns = {"/AjoutOperateur"})
 public class AjoutOperateur extends HttpServlet {
-
+public void Ajout(){
+    
+}
     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
@@ -37,6 +39,7 @@ public class AjoutOperateur extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
+    
         String loginBDD = null;
         String page = request.getParameter("page");
         OperateurService operateurORMService = metier.MetierFactory.getOperateurServ();
@@ -71,44 +74,5 @@ public class AjoutOperateur extends HttpServlet {
             page = "/erreurConnexion.jsp";
             request.setAttribute("erreurPage", "erreurMdpPasEgal");
         }
-    }  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 }
--- src/java/client/ProjetControler.java
+++ src/java/client/ProjetControler.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import metier.Operateur;
 import metier.OperateurService;
 import physique.data.*;
+import client.AjoutOperateur;
 
 /**
  *
@@ -22,7 +23,7 @@ import physique.data.*;
 public class ProjetControler extends HttpServlet {
 
     private OperateurORMService operateurORMSrv = PhysiqueDataFactory.getOperateurORMSrv();
-
+    AjoutOperateur ajoutOperateurSrv = new AjoutOperateur();
     /**
      * Processes requests for both HTTP
      * <code>GET</code> and
@@ -127,39 +128,39 @@ public class ProjetControler extends HttpServlet {
 
         /*   Xx Début Ajout opérateur xX  */
         if ("Ajout".equals(doAction)) {
-            
-//            OperateurService operateurORMService = metier.MetierFactory.getOperateurServ();
-//            Operateur operateur = new Operateur();
-//
-//            String nomInscription = request.getParameter("nomInscription");
-//            String prenomInscription = request.getParameter("prenomInscription");
-//            String loginInscription = request.getParameter("loginInscription");
-//            String mdpInscription = request.getParameter("mdpInscription");
-//            String confirmMdpInscription = request.getParameter("confirmMdpInscription");
-//            boolean isAdminInscription = false;
-//            try {
-//                loginBDD = metier.MetierFactory.getOperateurServ().getByLogin(loginInscription).getLogin();
-//            } catch (Exception ex) {
-//                Logger.getLogger(ProjetControler.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//            if (loginInscription.equals(loginBDD)) {
-//                page = "/erreurConnexion.jsp";
-//                request.setAttribute("erreurPage", "loginExistant");
-//            } else if (mdpInscription.equals(confirmMdpInscription)) {
-//                operateur.setNom(nomInscription);
-//                operateur.setPrenom(prenomInscription);
-//                operateur.setLogin(loginInscription);
-//                operateur.setMdp(mdpInscription);
-//                operateur.setAdmin(isAdminInscription);
-//                try {
-//                    operateurORMService.addOperateur(operateur);
-//                } catch (Exception ex) {
-//                    Logger.getLogger(ProjetControler.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//            } else {
-//                page = "/erreurConnexion.jsp";
-//                request.setAttribute("erreurPage", "erreurMdpPasEgal");
-//            }
+           
+            OperateurService operateurORMService = metier.MetierFactory.getOperateurServ();
+            Operateur operateur = new Operateur();
+
+            String nomInscription = request.getParameter("nomInscription");
+            String prenomInscription = request.getParameter("prenomInscription");
+            String loginInscription = request.getParameter("loginInscription");
+            String mdpInscription = request.getParameter("mdpInscription");
+            String confirmMdpInscription = request.getParameter("confirmMdpInscription");
+            boolean isAdminInscription = false;
+            try {
+                loginBDD = metier.MetierFactory.getOperateurServ().getByLogin(loginInscription).getLogin();
+            } catch (Exception ex) {
+                Logger.getLogger(ProjetControler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            if (loginInscription.equals(loginBDD)) {
+                page = "/erreurConnexion.jsp";
+                request.setAttribute("erreurPage", "loginExistant");
+            } else if (mdpInscription.equals(confirmMdpInscription)) {
+                operateur.setNom(nomInscription);
+                operateur.setPrenom(prenomInscription);
+                operateur.setLogin(loginInscription);
+                operateur.setMdp(mdpInscription);
+                operateur.setAdmin(isAdminInscription);
+                try {
+                    operateurORMService.addOperateur(operateur);
+                } catch (Exception ex) {
+                    Logger.getLogger(ProjetControler.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else {
+                page = "/erreurConnexion.jsp";
+                request.setAttribute("erreurPage", "erreurMdpPasEgal");
+            }
         }
         /*   Xx Debut SupprimerRapport xX  */
         if ("Supprimer rapport".equals(doAction)) {
